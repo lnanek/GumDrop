1f2f9a65c3fd5d21d2bf839f8ae24289069f4e8f
Lance Nanek <lnanek@gmail.com>
2014-02-21 16:50:12 -0800

    Expedia integration.

--- travelsight.js
+++ travelsight.js
@@ -1,12 +1,12 @@
 
-var currentNeighborhood;
+var currentNeighborhood = 'Bernal Heights';
 
 // Setup Swipe library to swipe through options.
 var elem = document.getElementById('mySwipe');
 window.mySwipe = Swipe(elem, {
   callback: function(index, element) {
 		currentNeighborhood = $(element).find('b').text();
-		alert('currentNeighborhood = ' + currentNeighborhood);
+		//alert('currentNeighborhood = ' + currentNeighborhood);
   }
   // startSlide: 4,
   // auto: 3000,
@@ -25,4 +25,9 @@ function findSchools() {
 
 	window.location = 'http://maps.google.com/?q=' + encodeURIComponent(queryValue);
 
+}
+
+function findHotels() {
+	window.location = 'http://www.travelnow.com/templates/454616/hotels/list?targetId=AREA-83820DD3-51C3-4B10-B044-A4D92B40AC1C%7Ccities';
+	
 }
