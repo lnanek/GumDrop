9b9f3bba864d0664d8e51c378c618c75b97a3e9f
Lance Nanek <lnanek@gmail.com>
2014-02-21 19:28:29 -0800

    menu

--- travelsight.js
+++ travelsight.js
@@ -19,15 +19,55 @@ window.mySwipe = Swipe(elem, {
 // alternate way with jQuery
 // window.mySwipe = $('#mySwipe').Swipe().data('Swipe');
 
+$('.hoverOnOffSrc').hover(
+	function() {
+		var onsrc = $( this ).attr('onsrc');
+		$( this ).attr('src', onsrc);
+	},
+	function() {
+		var offsrc = $( this ).attr('offsrc');
+		$( this ).attr('src', offsrc);	
+	}
+);
+
 function findSchools() {
 
-	var queryValue = 'schools near ' + currentNeighborhood;
+	// TODO use ESRI in future?
+
+	var queryValue = 'schools near ' + currentNeighborhood + ', San Francisco, CA, USA';
 
 	window.location = 'http://maps.google.com/?q=' + encodeURIComponent(queryValue);
 
 }
 
 function findHotels() {
+
+	// TODO flights too?
+	
+	// TODO expedia also has points of interest
+
 	window.location = 'http://www.travelnow.com/templates/454616/hotels/list?targetId=AREA-83820DD3-51C3-4B10-B044-A4D92B40AC1C%7Ccities';
 	
+}
+
+function reserveTour() {
+	alert('TODO with bitpay');
+}
+
+function furnish() {
+	alert('TODO with macys');
+}
+
+function findApartments() {
+	alert('TODO');
+}
+
+function like() {
+	$('#mySwipe').hide();
+	$('#likeOptions').show();
+}
+
+function closeLikeOptions() {
+	$('#mySwipe').show();
+	$('#likeOptions').hide();
 }
