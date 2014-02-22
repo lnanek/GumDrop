b99fb5325225dce4f265b0e3933d7adbd0ee7d49
Lance Nanek <lnanek@gmail.com>
2014-02-22 11:16:52 -0800

    Macy's

--- travelsight.js
+++ travelsight.js
@@ -19,6 +19,7 @@ window.mySwipe = Swipe(elem, {
 // alternate way with jQuery
 // window.mySwipe = $('#mySwipe').Swipe().data('Swipe');
 
+// Change buttons images when mouse rolls over them.
 $('.hoverOnOffSrc').hover(
 	function() {
 		var onsrc = $( this ).attr('onsrc');
@@ -29,6 +30,65 @@ $('.hoverOnOffSrc').hover(
 		$( this ).attr('src', offsrc);	
 	}
 );
+
+	$.ajax({
+			headers: { 
+					Accept : "application/json",
+					"X-Macys-Webservice-Client-Id" : "hackathon"
+			},
+			type:"GET",
+			url: "http://api.macys.com/v3/catalog/category/29391/browseproducts",
+			error: onMacysError,
+			success: onMacysResults
+	});
+    
+function onMacysError() {
+		// TODO offer retry
+    $('#macysResults').empty();    
+    var seeMoreLink = 
+    	$('<a>', { className: 'macysSeeMoreUrl' } )
+          .text("Sorry, there was a problem loading Macy's data!");          
+    $('#macysResults').append(seeMoreLink);
+    
+}    
+    
+function onMacysResults(data, status) {
+
+		//var str = JSON.stringify(data, undefined, 2);
+    //alert("data: " + data + ", status: " + status);
+    //$('#results').text(str);
+            
+    $('#macysResults').empty();
+            
+    // Add button each product returned
+    var productArray = data.category[0].product.product;    
+    for( var i=0; i < productArray.length && i < 6 ; i++ ) {
+        var product = productArray[i];
+        
+		    var productLink = $('<a>', { className: 'macysProductUrl', href: product.summary.producturl } );
+
+        var productImage = $('<img>', { className: 'macysProductImage', src: product.image[0].imageurl } );
+        productLink.append(productImage);
+        
+        var productText = $('<p>');
+        productText.text(product.summary.name);        
+        productLink.append(productText);
+        
+        productLink.append( $( '<div style="clear:both" ></div>' ) );
+        	            
+    		$('#macysResults').append(productLink);
+    		//$('#macysResults').append( $('<br />') );
+        
+    		//alert(product.summary.name);
+    }
+    
+    // Add link to more items in category
+    var seeMoreUrl = data.category[0].summary.categorypageurl;
+    var seeMoreLink = 
+    	$('<a>', { className: 'macysSeeMoreUrl', href: seeMoreUrl } )
+          .text('See More');          
+    $('#macysResults').append(seeMoreLink);
+}
 
 function findSchools() {
 
@@ -42,32 +102,36 @@ function findSchools() {
 
 function findHotels() {
 
-	// TODO flights too?
+	// TODO flights from expedia
 	
-	// TODO expedia also has points of interest
+	// TODO points of interest and photos from expedia
 
 	window.location = 'http://www.travelnow.com/templates/454616/hotels/list?targetId=AREA-83820DD3-51C3-4B10-B044-A4D92B40AC1C%7Ccities';
 	
 }
 
 function reserveTour() {
-	alert('TODO with bitpay');
+
+	// TODO use server API to qualify for better prize?
+
+	window.location = 'https://bitpay.com/cart/add?itemId=LGYdVphHQ4XJacR2kms4tQ';
 }
 
 function furnish() {
-	alert('TODO with macys');
+	$('.page').hide();
+	$('#macysPage').show();
 }
 
 function findApartments() {
-	alert('TODO');
+	window.location = 'http://sfbay.craigslist.org/apa/';
 }
 
-function like() {
-	$('#mySwipe').hide();
+function showLikeOptions() {
+	$('.page').hide();
 	$('#likeOptions').show();
 }
 
 function closeLikeOptions() {
+	$('.page').hide();
 	$('#mySwipe').show();
-	$('#likeOptions').hide();
 }
